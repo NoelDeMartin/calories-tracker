cd4a2deccda97751b7867675e30154bc661b4b3d
Noel De Martin <noel@noeldemartin.com>
2025-04-10 20:44:24 +0200

    Update boilerplate

--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./index.html', './src/**/*.{vue,ts}', './node_modules/@aerogel/core/dist/**/*.js'],
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-};
